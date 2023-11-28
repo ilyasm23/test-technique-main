41e0898ee8269f64241dcf9666a93aab060428bf
Ilyas Mamou <ilyas.mamou23@gmail.com>
2023-11-28 12:49:01 +0100

    Update tests
    Update ci.yml
    
    Update ci.yml
    
    Update ci.yml
    
    Update ci.yml
    
    Update tests and retry CI

--- frontend/tests/signin.spec.ts
+++ frontend/tests/signin.spec.ts
@@ -3,10 +3,18 @@ import { test, expect } from '@playwright/test';
 
 test('can sign in', async ({ page }) => {
   await page.goto(process.env.VUE_APP_BASE_URL);
+  await expect(
+    page.locator('form div').filter({ hasText: 'Connecter' }),
+  ).toBeVisible();
   await page.getByLabel('Email*').fill('toto@kresus.eu');
   await page.getByLabel('Mot de passe*').fill('test');
+  await expect(page.getByLabel('Email*')).toBeVisible();
   await page.getByRole('button', { name: 'Connecter' }).click();
-  await expect(page.getByRole('banner')).toContainText(
-    'Une TODO app pas comme les autres',
-  );
+  // await expect(page.getByRole('banner')).toContainText(
+  //   'Une TODO app pas comme les autres',
+  // );
+  await expect(
+    page.getByRole('button', { name: 'Ajouter une tâche' }),
+  ).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Déconnexion' })).toBeVisible();
 });
